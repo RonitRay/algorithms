25e83801d4a1d831976da4e8a7e2a4910f69f536
Ronit Ray <ray.ronit@gmail.com>
2017-11-11 21:08:00 +0530

    insertion sort

---
+++ insertionsort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void insSort(int a[], int n)
+{
+	int i, key, j;
+   	for (i = 1; i < n; i++)
+   	{
+       	key = a[i];
+       	j = i-1;
+       	while (j >= 0 && a[j] > key)
+       	{
+           	a[j+1] = a[j];
+           	j = j-1;
+       	}
+       a[j+1] = key;
+   	}
+	
+	for(i=0;i<n;i++)
+		printf("%d ",a[i]);
+}
+
+void main()
+{
+	int a[]={5,3,2,4,1,6,9,8,7,10};
+	insSort(a,10);
+}
