9357f6916b783aa4d74ceaa609aadeac0af3ce2b
Ronit Ray <ray.ronit@gmail.com>
2017-11-12 16:09:30 +0530

    update

--- floydwarshall.c
+++ floydwarshall.c
@@ -2,7 +2,7 @@
 #define V 4
 #define INF 99999
 
-void printSolution(int dist[][V])
+void printSolution(int d[][V])
 {
     printf ("Following matrix shows the shortest distances"
             " between every pair of vertices \n");
@@ -10,21 +10,21 @@ void printSolution(int dist[][V])
     {
         for (int j = 0; j < V; j++)
         {
-            if (dist[i][j] == INF)
+            if (d[i][j] == INF)
                 printf("%7s", "INF");
             else
-                printf ("%7d", dist[i][j]);
+                printf ("%7d", d[i][j]);
         }
         printf("\n");
     }
 }
 
-void floydWarshall (int graph[][V])
+void floydWarshall (int g[][V])
 {
-    int dist[V][V], i, j, k;
+    int d[V][V], i, j, k;
     for (i = 0; i < V; i++)
         for (j = 0; j < V; j++)
-            dist[i][j] = graph[i][j];
+            d[i][j] = d[i][j];
  
     for (k = 0; k < V; k++)
     {
@@ -32,21 +32,21 @@ void floydWarshall (int graph[][V])
         {
             for (j = 0; j < V; j++)
             {
-                if (dist[i][k] + dist[k][j] < dist[i][j])
-                    dist[i][j] = dist[i][k] + dist[k][j];
+                if (d[i][k] + d[k][j] < d[i][j])
+                    d[i][j] = d[i][k] + d[k][j];
             }
         }
     }
-    printSolution(dist);
+    printSolution(d);
 }
  
 int main()
 {
-    int graph[V][V] = { {0,   5,  INF, 10},
-                        {INF, 0,   3, INF},
-                        {INF, INF, 0,   1},
-                        {INF, INF, INF, 0}
-                      };
-     floydWarshall(graph);
+    int g[V][V] = { {0,   5,  INF, 10},
+                    {INF, 0,   3, INF},
+                    {INF, INF, 0,   1},
+                    {INF, INF, INF, 0}
+                  };
+     floydWarshall(g);
     return 0;
 }
